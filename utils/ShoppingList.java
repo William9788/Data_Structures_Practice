402cecd2d0c55201403eeb90f38d1cb63ef7ff49
William9788 <hewohui9788@icloud.com>
2026-03-04 20:34:19 +0000

    feat:code implemented

--- utils/ShoppingList.java
+++ utils/ShoppingList.java
@@ -1,5 +1,25 @@
 package utils;
 public class ShoppingList {
-
-    
+/**
+ * This arraylist is for store string stuff
+ * @param command line not used
+*/
+    public static void main(String[] args){
+        DynamicArrayList list = new DynamicArrayList();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Candle");
+        list.add("Dog");
+        list.add("Eggs");
+        list.add("Gun");
+        list.add("Hat");
+        list.add("Jacket");
+        list.add("Keyboard");
+        list.add("Lime");
+        for(int i =0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        int position = list.indexOf("Apple");
+        System.out.println("Apple position:"+ position);
+    }
 }
